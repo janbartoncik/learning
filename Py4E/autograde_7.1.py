119b1757b0fd3356dd69fc0fe4fccb55683b40a0
janbartoncik <jan.bartoncik@yahoo.com>
2022-12-04 18:51:22 +0100

    update

--- Py4E/autograde_7.1.py
+++ Py4E/autograde_7.1.py
@@ -1,3 +1,4 @@
+# Write a program that prompts for a file name, then opens that file and reads through the file, and print the contents of the file in upper case.
 # Use words.txt as the file name
 fname = input("Enter file name: ")
 fh = open(fname)
