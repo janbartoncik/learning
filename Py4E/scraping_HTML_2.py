4c98160873a4840182fe430b35f1f84b7fd3fe3b
janbartoncik <jan.bartoncik@yahoo.com>
2022-12-31 01:06:28 +0100

    Update scraping_HTML_2.py

--- Py4E/scraping_HTML_2.py
+++ Py4E/scraping_HTML_2.py
@@ -1,4 +1,5 @@
-# The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file. 
+# The program will use urllib to read the HTML from the data files below, extract the href= vaues from the anchor tags, scan for a tag that is in a particular position relative to the first name in the list, follow that link and repeat the process a number of times and report the last name you find. 
+# Start at http://py4e-data.dr-chuck.net/known_by_Torran.html
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -10,15 +11,18 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = 'http://py4e-data.dr-chuck.net/known_by_Torran.html'
-html = urlopen(url, context=ctx).read()
-soup = BeautifulSoup(html, "html.parser")
-i=1
+pos = int(input())-1
+times = int(input())
+url = input()
 
-while i < 3:
+for idk in range(times):
+    lst = list()
+    html = urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, "html.parser")
+
     tags = soup('a')
     for tag in tags:
-        print(tag.get('href', None))
-        i= i+1
-
-# použít while funkci 2x
+        o = tag.get('href', None)
+        lst.append(o)
+    print(lst[pos])
+    url = lst[pos]
